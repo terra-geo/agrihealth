089013a532b025e18e272150bc71d9da329759c8
Maria <climatealerta@gmail.com>
2025-03-16 16:38:50 +0000

    commit 2

---
+++ docs/index.md
@@ -0,0 +1,16 @@
+# Welcome to agrihealth
+
+
+[![image](https://img.shields.io/pypi/v/agrihealth.svg)](https://pypi.python.org/pypi/agrihealth)
+
+
+**agrihealth is a Python package that allows fa**
+
+
+-   Free software: MIT License
+-   Documentation: <https://terra-geo.github.io/agrihealth>
+    
+
+## Features
+
+-   TODO
---
+++ tests/test_agrihealth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `agrihealth` package."""
+
+
+import unittest
+
+from agrihealth import agrihealth
+
+
+class TestAgrihealth(unittest.TestCase):
+    """Tests for `agrihealth` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
